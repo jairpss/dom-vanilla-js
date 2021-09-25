e7ce7536f885a003a48ff0c5b3c7756a9104098f
Jair Partida <jairpartida1@gmail.com>
2021-09-24 22:21:13 -0600

    create a counter

--- app.js
+++ app.js
@@ -6,11 +6,34 @@
 
 // parrafo.classList.add('p1'); //classList.add agrega una clase
 
+// const lista = document.getElementById('lista');
 
-const lista = document.getElementById('lista');
-console.log(lista);
+// const arr = ['primer elemento', 'segundo', 'tercero'];
 
-const li = document.createElement('li');
-li.textContent = 'primer elemento de lista';
+// //Primera opcion
 
-lista.appendChild(li);
+// // arr.forEach(item => {
+// //     const li = document.createElement('li');  //createElement: crea un elemento html, por ej: li
+// //     li.textContent = item;
+// //     lista.appendChild(li); //crea un li dentro de la lista 
+// // })
+
+// //Segunda opcion con template literals
+// arr.forEach(item => {
+//     lista.innerHTML += `<li>${item}</li>`;
+// })
+
+const btnAumentar = document.querySelector('.btn-info');
+const btnDisminuir = document.querySelector('.btn-danger');
+const span = document.getElementById('resultado');
+let contador = 0;
+
+btnAumentar.addEventListener('click', () => {
+    contador++;
+    span.textContent = contador;
+})
+
+btnDisminuir.addEventListener('click', () => {
+    contador--;
+    span.textContent = contador;
+})
