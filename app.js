b734a11714d50cd1e3d971b5aa818f77bf67e13c
Jair Partida <jairpartida1@gmail.com>
2021-09-27 22:45:14 -0600

    adding product to cart

--- app.js
+++ app.js
@@ -39,12 +39,21 @@
 //     span.textContent = contador;
 // })
 
+const cards = document.getElementById('cards');
 const items = document.getElementById('items');
+const footer = document.getElementById('footer');
 const templateCard = document.getElementById('template-card').content;
+const templateFooter = document.getElementById('template-footer').content;
+const templateCarrito = document.getElementById('template-carrito').content;
 const fragment = document.createDocumentFragment();
+let carrito = {}
 
 document.addEventListener('DOMContentLoaded', () => {
     fetchData();
+});
+
+cards.addEventListener('click', e => {
+    addCarrito(e);
 });
 
 const fetchData = async () => {
@@ -60,9 +69,34 @@ const fetchData = async () => {
 const pintarCards = data => {
     data.forEach(producto => {
         templateCard.querySelector('h5').textContent = producto.title;
-
+        templateCard.querySelector('p').textContent = producto.precio;
+        //templateCard.querySelector('img').SetAttribute("src", producto.thumbnailUrl);
+        templateCard.querySelector('.btn-dark').dataset.id = producto.id;
         const clone = templateCard.cloneNode(true);
         fragment.appendChild(clone);
     })
     items.appendChild(fragment);
+}
+
+// Agregar al carrito
+const addCarrito = e => {
+    if (e.target.classList.contains('btn-dark')) {
+        setCarrito(e.target.parentElement)
+    }
+    e.stopPropagation();
+}
+
+const setCarrito = objeto => {
+    const producto = {
+        id: objeto.querySelector('.btn-dark').dataset.id,
+        title: objeto.querySelector('h5').textContent,
+        precio: objeto.querySelector('p').textContent,
+        cantidad: 1
+    }
+
+    if (carrito.hasOwnProperty(producto.id)) {
+        producto.cantidad = carrito[producto.id].cantidad + 1
+    }
+
+    carrito[producto.id] = {...producto}
 }
