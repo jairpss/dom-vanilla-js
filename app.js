515241c32f96dd95fca84ff062f3551e3e4188b0
Jair Partida <jairpartida1@gmail.com>
2021-09-28 21:34:32 -0600

    reduce method and empty cart

--- app.js
+++ app.js
@@ -134,4 +134,22 @@ const pintarFooter = () => {
         `
         return
     }
+
+    // sumar cantidad y sumar totales
+    const nCantidad = Object.values(carrito).reduce((acc, { cantidad }) => acc + cantidad, 0);
+    const nPrecio = Object.values(carrito).reduce((acc, {cantidad, precio}) => acc + cantidad * precio , 0);
+    // console.log(nPrecio)
+
+    templateFooter.querySelectorAll('td')[0].textContent = nCantidad;
+    templateFooter.querySelector('span').textContent = nPrecio;
+
+    const clone = templateFooter.cloneNode(true);
+    fragment.appendChild(clone);
+    footer.appendChild(fragment);
+
+    const btnVaciar = document.getElementById('vaciar-carrito');
+    btnVaciar.addEventListener('click', () => {
+        carrito = {};
+        pintarCarrito();
+    })
 }
